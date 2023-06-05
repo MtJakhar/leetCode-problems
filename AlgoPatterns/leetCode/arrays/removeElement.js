1caed268998ff4253d7fe4a929776b1b61a5db8b
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-06-05 12:23:18 -0400

    edited with new solution

--- AlgoPatterns/leetCode/arrays/removeElement.js
+++ AlgoPatterns/leetCode/arrays/removeElement.js
@@ -6,6 +6,31 @@
 // Input: nums = [3,2,2,3], val = 3
 // Output: 2, nums = [2,2,_,_]
 
+const removeElement3 = (nums, val) => {
+  let index = 0;
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[index] = nums[i];
+      index++;
+    }
+  }
+  return index;
+};
+
+const removeElement4 = (nums, val) => {
+  let k = 0; // Represents the number of elements not equal to val
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      // If the element is not equal to val, move it to the front of the array
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k; // Return the number of elements not equal to val
+};
+
 // my first version, creating my own splice method timeC => O(N^2) doesn't work cause leetcode is retarded and is asking for the answer in the dumbest way possible.
 let array = [3, 2, 2, 3];
 
@@ -41,28 +66,3 @@ const removeElement2 = (nums, val) => {
 console.log(removeElement2(array, 3));
 
 // best answer works timeC => O(n) SpaceC = O(1)
-
-const removeElement3 = (nums, val) => {
-  let index = 0;
-  for (i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      nums[index] = nums[i];
-      index++;
-    }
-  }
-  return index;
-};
-
-const removeElement4 = (nums, val) => {
-  let k = 0; // Represents the number of elements not equal to val
-
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      // If the element is not equal to val, move it to the front of the array
-      nums[k] = nums[i];
-      k++;
-    }
-  }
-
-  return k; // Return the number of elements not equal to val
-};
