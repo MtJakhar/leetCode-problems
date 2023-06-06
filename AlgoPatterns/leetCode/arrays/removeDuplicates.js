e86a80967b3433b79e4de1981b8ab6d7f76af2db
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-06-06 11:27:36 -0400

    added comments

--- AlgoPatterns/leetCode/arrays/removeDuplicates.js
+++ AlgoPatterns/leetCode/arrays/removeDuplicates.js
@@ -1,16 +1,23 @@
 const removeDuplicates = (nums) => {
-  //     create pointer at index 0
-  let pointer = 0;
-  //     create loop which loop through array and compares each number to pointer
-  for (index = 1; index < nums.length; index++) {
-    //         compare number to pointer if it is not the same move index, if num is diff, move pointer and change next index over to that unique value
-    if (nums[pointer] !== nums[index]) {
-      pointer++;
-      nums[pointer] = nums[index];
-    }
+  if (nums.length === 0) {
+    return 0; // If the array is empty, there are no duplicates to remove
   }
-  //     return amount of unique values
-  return pointer + 1;
+
+  let i = 0; // Pointer to keep track of the last index with a unique value
+
+  for (let index = 1; index < nums.length; index++) {
+    // Iterate through the array starting from the second element
+
+    if (nums[index] !== nums[i]) {
+      // If the current element is different from the last unique element
+
+      i++; // Increment the pointer to move to the next position
+      nums[i] = nums[index]; // Place the distinct element in the next position
+    }
+    // If the current element is a duplicate, skip it and continue to the next iteration
+  }
+
+  return i + 1; // Return the count of unique elements (i + 1)
 };
 
 // time complexity is O(n), while the space is O(1)
