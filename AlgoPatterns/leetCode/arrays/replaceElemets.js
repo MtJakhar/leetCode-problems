5eddb926ba63ab447f5fbbd07133f10a8890621b
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-06-07 13:26:42 -0400

    added notes

--- AlgoPatterns/leetCode/arrays/replaceElemets.js
+++ AlgoPatterns/leetCode/arrays/replaceElemets.js
@@ -7,10 +7,15 @@ let arr1 = [17, 18, 5, 4, 6, 1];
 let arr2 = [18, 6, 6, 6, 1, -1];
 
 const replaceElements = (arr) => {
+  // create variable with a value of -1
   let rightMax = -1;
+  // loop through array from the end to the beginning
   for (let i = arr.length - 1; i >= 0; i--) {
+    // create variable which which compares rightmax with current index, choose the greater of the two
     let newMax = Math.max(arr[i], rightMax);
+    // set current variable to rightMax
     arr[i] = rightMax;
+    // let rightmax equal to the newmax
     rightMax = newMax;
   }
   return arr;
