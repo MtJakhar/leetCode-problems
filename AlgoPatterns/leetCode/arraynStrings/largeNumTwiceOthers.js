4dd48b919ce3ace6df795f1267f5d055cf26a892
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-06-13 15:34:25 -0400

    reviewed problem

--- AlgoPatterns/leetCode/arraynStrings/largeNumTwiceOthers.js
+++ AlgoPatterns/leetCode/arraynStrings/largeNumTwiceOthers.js
@@ -26,3 +26,27 @@ const dominantIndex = (nums) => {
   // return largestIndex if for loops are completed.
   return largeIndex;
 };
+
+// nums = [3,6,1,0]
+
+const dominantIndex2 = (nums) => {
+  let largest = 0;
+  let largeIdx = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > largest) {
+      largest = nums[i];
+      largeIdx = i;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i === largeIdx) {
+      i++;
+    }
+    if (largest < nums[i] * 2) {
+      return -1;
+    }
+  }
+  return largeIdx;
+};
