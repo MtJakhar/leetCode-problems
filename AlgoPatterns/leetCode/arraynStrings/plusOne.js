c2c4d131728852ccad395aefccc1adbd621eeb35
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-06-14 13:09:22 -0400

    solved leetcode problem plusOne.

--- AlgoPatterns/leetCode/arraynStrings/plusOne.js
+++ AlgoPatterns/leetCode/arraynStrings/plusOne.js
@@ -1,6 +1,6 @@
 // timeC = O(n) spaceC = o(n)
-
-const plusOne = (digits) => {
+// this was my first attempt at solving this. It didnot pass all test cases.
+const plusOneBeta = (digits) => {
   let string = "";
 
   for (let i = 0; i < digits.length; i++) {
@@ -18,5 +18,21 @@ const plusOne = (digits) => {
     newdigits.push(parseInt(string[i]));
   }
 
-  console.log(newdigits);
+  return newdigits;
+};
+
+const plusOne = (digits) => {
+  // loop through the array starting from the rear
+  for (let i = digits.length - 1; i >= 0; i--) {
+    // if digit is less than 9 add 1 and return updated array;
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    // becuase current digit is 9, set it to zero
+    digits[i] = 0;
+  }
+  // if all digits are 9 , we need to add a 1 to the beginning of the array.
+  digits.unshift(1);
+  return digits;
 };
