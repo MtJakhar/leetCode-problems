aafe63f627f0e9fc86033f4a460709358e8452f7
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-06-26 13:29:47 -0400

    added another solution to common prefix

--- AlgoPatterns/leetCode/arraynStrings/longestCommonPrefix.js
+++ AlgoPatterns/leetCode/arraynStrings/longestCommonPrefix.js
@@ -22,3 +22,55 @@ function longestCommonPrefix(strs) {
 
   return prefix;
 }
+// tc = O(s) sc = O(1)
+
+const longestCommonPrefix2 = (strs) => {
+  // If the input array is empty, there is no common prefix, so return an empty string
+  if (strs.length === 0) {
+    return "";
+  }
+
+  // Sort the input array of strings in lexicographical order
+  strs.sort();
+
+  // Get the first and last string from the sorted array
+  const firstStr = strs[0];
+  const lastStr = strs[strs.length - 1];
+
+  // Find the common prefix between the first and last strings
+  let commonPrefix = "";
+  for (let i = 0; i < firstStr.length; i++) {
+    if (firstStr[i] === lastStr[i]) {
+      // If the characters match, add it to the common prefix
+      commonPrefix += firstStr[i];
+    } else {
+      // If the characters don't match, break the loop
+      break;
+    }
+  }
+
+  return commonPrefix;
+};
+// tc = O(n*m) sc = o(1)
+
+const longestCommonPrefix3 = (strs) => {
+  // check for edge case of no strsings
+  if (strs.length === 0) return "";
+  // sort based on alpha order
+  strs.sort();
+  // crete var for first and last strsing, if first and last share common prefix all will cause it is in alpha order
+  let first = strs[0];
+  let last = strs[strs.length - 1];
+
+  // create var for common prefix as empty strsing
+  let cPrefix = "";
+  for (let i = 0; i < first.length; i++) {
+    // check to see if first and last current index are the same, if yes add to cPrefix, else break
+    if (first[i] === last[i]) {
+      cPrefix += first[i];
+    } else {
+      break;
+    }
+  }
+  return cPrefix;
+};
